a80a285c9c88aed86c376a8d4672b45349ca25dc
Maarten Bicknese <m.bicknese@cardano.com>
2025-01-07 08:27:02 +0100

    feat: add simple graphql interface

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from ariadne import load_schema_from_path, make_executable_schema
+from ariadne.asgi import GraphQL
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,3 +13,14 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+# Load schema from file...
+root_defs = load_schema_from_path("./api/root.graphql")
+client_defs = load_schema_from_path("./api/client/schema.graphql")
+config_defs = load_schema_from_path("./api/config/schema.graphql")
+
+# Build an executable schema
+schema = make_executable_schema([root_defs, client_defs, config_defs])
+
+app.mount("/graphql/", GraphQL(schema, debug=True))
